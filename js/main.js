a37873784367252fbb5361d4d82e4dd2bb3ea07b
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-12-06 00:25:59 +0500

    add init test

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+function nextEdge(side1, side2) {
+	return (side1+side2)-1
+// ---------------------------Masala-1-------------------
+function nextEdge(a,b) {
+	return a+b;
+	
+}
+
+
+// ----------------------------Masala-2--------------------------
+
+    function convert(minutes) {
+        return minutes*60;
+    }
+	
+}
+// -----------------------------Masala-3------------------------
+function addition(num) {
+	return num+1;
+}
+
+// -------------------------------Masala-4------------------------
+function triArea(base, height) {
+	return (base*height)/2
+}
+// --------------------------------Masala-5--------------------------
+function calcAge(age) {
+	return age*365;
+}
+// ---------------------------------Masala-6-------------------------
+function remainder(x, y) {
+	return x%y;
+}
+// ----------------------------------Masala-7---------------------------
+function footballPoints(wins, draws, losses) {
+	return wins*3+draws*1;
+}
+// ------------------------------------Masala-8-------------------------
+function points(twoPointers, threePointers) {
+	return twoPointers*2+threePointers*3 
+}
+// ------------------------------------Masala-9--------------------------
+function lessThan100(a, b) {
+	return a+b<100 ?true:false;
+}
+// -------------------------------------Masala-10-----------------------------
+function animals(chickens, cows, pigs) {
+	return chickens*2+cows*4+pigs*4;
+}
